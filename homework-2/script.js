6dbc16844acea12f0e957e5e580e3d7f95df5dff
unknown <bielievtsovdmitriy@gmail.com>
2022-09-27 14:41:35 +0300

    add homework 2

---
+++ homework-2/script.js
@@ -0,0 +1,27 @@
+// First exercise:
+let userAge;
+let userName = "Dmitriy";
+let userSurname = "Bielievtsov";
+
+// Second exercise:
+let newUser;
+let shoppingList;
+let userOnlineStatus;
+let employeeSalary;
+let cursorCoordinates;
+
+// third exercise:
+let hello = "Hello World!";
+console.log(hello);
+
+
+userAge = 25;
+console.log("userAge", userAge);
+console.log("typeof userAge", typeof userAge);
+
+console.log("userName", userName);
+
+console.log("userSurname", userSurname);
+
+
+

226da5970be009788cedbd113bab57ddb1d2ee9f
unknown <bielievtsovdmitriy@gmail.com>
2022-09-30 21:08:02 +0300

    fixed exercise 2

--- homework-3/script.js
+++ homework-3/script.js
@@ -1,37 +1,37 @@
 // exercise 1:
 
-let firstValue = Number(prompt("Введите первое число"));
-let secondValue = Number(prompt("Введите второе число"));
-if (firstValue > secondValue){
-    alert(`Число ${firstValue} больше, чем число ${secondValue}`);
-}
-else if(firstValue < secondValue){
-    alert(`Число ${firstValue} меньше, чем число ${secondValue}`);
-}
-else if( firstValue === secondValue){
-    alert("Числа равны");
-}
-else{
-    alert("Неизвестное значение. Повторите попытку.");
-}
+// let firstValue = Number(prompt("Введите первое число"));
+// let secondValue = Number(prompt("Введите второе число"));
+// if (firstValue > secondValue){
+//     alert(`Число ${firstValue} больше, чем число ${secondValue}`);
+// }
+// else if(firstValue < secondValue){
+//     alert(`Число ${firstValue} меньше, чем число ${secondValue}`);
+// }
+// else if( firstValue === secondValue){
+//     alert("Числа равны");
+// }
+// else{
+//     alert("Неизвестное значение. Повторите попытку.");
+// }
 
 
 // exercise 2:
 
-// let firstValue = Number(prompt("Введите первое число"));
-// let secondValue = Number(prompt("Введите второе число"));
-let operation = Number(prompt("Выберите Операцию:\n 1.Суммирование\n 2.Вычитание\n 3.Деление\n 4.Умножение"));
+let firstValue = Number(prompt("Введите первое число"));
+let secondValue = Number(prompt("Введите второе число"));
+let operation = prompt("Выберите Операцию: + - / *");
 switch (operation) {
-    case 1:
+    case '+':
         alert( `${firstValue} + ${secondValue} = ${firstValue + secondValue}`);
         break;
-    case 2:
+    case '-':
         alert( `${firstValue} - ${secondValue} = ${firstValue - secondValue}`);
          break;
-    case 3:
+    case '/':
         alert( `${firstValue} / ${secondValue} = ${firstValue / secondValue}`);
         break;
-    case 4:
+    case '*':
         alert( `${firstValue} * ${secondValue} = ${firstValue * secondValue}`);
         break;
     default:
@@ -42,22 +42,22 @@ switch (operation) {
 
 // exercise 3:
 
-let visitorAge = Number(prompt("Введите ваш возраст"));
-if (visitorAge >= 60 && visitorAge <= 80 || visitorAge >= 12 && visitorAge < 18) {
-    let clarification = confirm("Вам разрешили взрослые?");
-    if (clarification) {
-        alert("Вход разрешен");
-    }
-     else {
-        alert("Извине, вход запрещен");
-    }
-}
- else if (visitorAge < 12 || visitorAge > 80) {
-    alert("Извине, вход запрещен");
-}
- else if (visitorAge >= 18 && visitorAge < 60){
-    alert("Взрослым вход разрешен");
- }
- else{
-    alert("Неизвестное значение");
- }
+// let visitorAge = Number(prompt("Введите ваш возраст"));
+// if (visitorAge >= 60 && visitorAge <= 80 || visitorAge >= 12 && visitorAge < 18) {
+//     let clarification = confirm("Вам разрешили взрослые?");
+//     if (clarification) {
+//         alert("Вход разрешен");
+//     }
+//      else {
+//         alert("Извине, вход запрещен");
+//     }
+// }
+//  else if (visitorAge < 12 || visitorAge > 80) {
+//     alert("Извине, вход запрещен");
+// }
+//  else if (visitorAge >= 18 && visitorAge < 60){
+//     alert("Взрослым вход разрешен");
+//  }
+//  else{
+//     alert("Неизвестное значение");
+//  }

899883022fa55781051d0367e16360a6395076ea
unknown <bielievtsovdmitriy@gmail.com>
2022-10-27 18:00:57 +0300

    finished homework 9

--- homework-9/script.js
+++ homework-9/script.js
@@ -1,63 +1,77 @@
-const data = [
-  {
-    name: "John",
-    age: 24,
-    position: "senior",
-    isActive: false,
-  },
-  {
-    name: "Peter",
-    age: 33,
-    position: "middle",
-    isActive: false,
-  },
-  {
-    name: "Sam",
-    age: 29,
-    position: "junior",
-    isActive: true,
-  },
-  {
-    name: "Mary",
-    age: 24,
-    position: "middle",
-    isActive: false,
-  },
-  {
-    name: "Steve",
-    age: 23,
-    position: "middle",
-    isActive: true,
-  },
-  {
-    name: "Kate",
-    age: 31,
-    position: "middle",
-    isActive: false,
-  },
-  {
-    name: "Sally",
-    age: 19,
-    position: "junior",
-    isActive: false,
-  },
-  {
-    name: "Jack",
-    age: 19,
-    position: "middle",
-    isActive: true,
-  },
-];
+//  Задание 1
 
-function filterData(arr, obj) {
-  const keys = Object.entries(obj);
-  return arr.filter((person) => keys.every(([key, val]) => person[key] === val));
-}
-let res = filterData(data, {age: 19, position: "junior" });
- console.log(res)
+//  1.1
 
 
+// let mapArray = (arr) => arr.map(number =>{
+//   if (number < 0){
+//     return false;
+//   }
+//   return true
+// })
 
+// let res = mapArray([1, 2, -10, -2, 7])
+// console.log(res)
+
+
+//  1.2
+
+const users = [
+  {
+    name: 'Sam',
+    role: 'admin',
+    age: 25,
+  },
+  {
+    name: 'Pablo',
+    role: 'admin',
+    age: 32,
+  },
+  {
+    name: 'Enrico',
+    role: 'client',
+    age: 43,
+  },
+  {
+    name: 'Mary',
+    role: 'client',
+    age: 34,
+  },
+  {
+  name: 'Peter',
+  role: 'admin',
+  age: 16,
+},
+    {
+    name: 'Katerine',
+    role: 'guest',
+    age: 17,
+  },
+]
+
+let getUsersAdmin = (arr) => arr
+.filter(person => person.age > 18 && person.role === 'admin')
+.map(person => {
+  return person.name;
+}) 
+
+const adultAdmins = getUsersAdmin(users);
+ console.log(adultAdmins);
+
+//  1.3
+
+
+// let getAverage = (numbers) => {
+//   let sum = numbers.reduce((acc, number) => acc + number, 0);
+//   let length = numbers.length;
+//   return sum / length;
+// };
+// const numbers = [1, 10, 17, 24, 45]
+// console.log(getAverage(numbers))
+// // console.log(Math.round(getAverage(numbers)))  //  Если результат должен быть округлён
+
+
+// Задание 2
 
 
 // const logger = (element, index, array) => {
@@ -100,3 +114,76 @@ let res = filterData(data, {age: 19, position: "junior" });
 // };
 // let result = ownFilter([-2, 4, -1], isNegative);
 // console.log(result)
+
+
+//  Задание 3
+
+
+// const data = [
+//   {
+//     name: "John",
+//     age: 24,
+//     position: "senior",
+//     isActive: false,
+//   },
+//   {
+//     name: "Peter",
+//     age: 33,
+//     position: "middle",
+//     isActive: false,
+//   },
+//   {
+//     name: "Sam",
+//     age: 29,
+//     position: "junior",
+//     isActive: true,
+//   },
+//   {
+//     name: "Mary",
+//     age: 24,
+//     position: "middle",
+//     isActive: false,
+//   },
+//   {
+//     name: "Steve",
+//     age: 23,
+//     position: "middle",
+//     isActive: true,
+//   },
+//   {
+//     name: "Kate",
+//     age: 31,
+//     position: "middle",
+//     isActive: false,
+//   },
+//   {
+//     name: "Sally",
+//     age: 19,
+//     position: "junior",
+//     isActive: false,
+//   },
+//   {
+//     name: "Jack",
+//     age: 19,
+//     position: "middle",
+//     isActive: true,
+//   },
+// ];
+
+// function filterData(arr, obj) {
+//   const keys = Object.entries(obj);
+//   return arr.filter((person) => keys.every(([key, val]) => person[key] === val));
+// }
+// let res = filterData(data, {age: 19, position: "junior" });
+//  console.log(res)
+
+
+
+
+
+
+
+
+
+
+

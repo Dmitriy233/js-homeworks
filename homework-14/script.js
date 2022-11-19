b883501a4bed64b72a7f9b2a16228fa6eb8e4f31
Dmitry Bielievtsov <bielievtsovdmitriy@gmail.com>
2022-11-19 14:12:41 +0200

    fixed homework 14

--- homework-14/script.js
+++ homework-14/script.js
@@ -31,55 +31,7 @@ const formConfig = [
   },
 ];
 
-const form = document.createElement('form');
-const formInputLogin = document.createElement('div');
-const formInputAge = document.createElement('div');
-const formSelect = document.createElement('div');
-let button = document.createElement('button');
-button.innerText = 'Продолжить';
-button.setAttribute('type', 'submit');
 
-const createInputs = () => {
-  formConfig.forEach(config => {
-    if (config.element === 'text') {
-      let inputs = document.createElement('input');
-      let inputsLabel = document.createElement('label');
-
-      inputs.setAttribute('type', config.element);
-      inputsLabel.setAttribute('for', config.name);
-      inputsLabel.innerHTML = config.label;
-      inputs.setAttribute('id', config.name);
-
-      if (config.name === 'login') {
-        formInputLogin.append(inputsLabel, inputs);
-      };
-
-      if (config.name === 'age') {
-        formInputAge.append(inputsLabel, inputs);
-      };
-    };
-  });
-};
-
-const createSelectAndOption = () => {
-  const findSelectElement = formConfig.find((config) => config.element === 'select');
-  let languageSelect = document.createElement('select');
-  let selectLabel = document.createElement('label');
-
-  selectLabel.setAttribute('for', findSelectElement.name);
-  selectLabel.innerHTML = findSelectElement.label;
-  languageSelect.setAttribute('id', findSelectElement.name);
-
-  findSelectElement.options.forEach(option => {
-    option.text;
-    const selectOptionJs = document.createElement('option');
-    selectOptionJs.innerHTML = option.text;
-    selectOptionJs.setAttribute('value', option.value);
-    languageSelect.append(selectOptionJs);
-  })
-
-  formSelect.append(selectLabel, languageSelect);
-};
 
 const handleSubmit = (event) => {
   event.preventDefault();
@@ -95,12 +47,86 @@ const handleSubmit = (event) => {
   console.log(formValues)
 };
 
-form.addEventListener('submit', handleSubmit);
-createInputs();
-createSelectAndOption();
 
-form.append(formInputLogin, formInputAge, formSelect, button);
-document.body.append(form);
+
+
+function createForm() {
+  const form = document.createElement('form');
+  const formInputLogin = document.createElement('div');
+  const formInputAge = document.createElement('div');
+  const formSelect = document.createElement('div');
+  let button = document.createElement('button');
+  button.innerText = 'Продолжить';
+  button.setAttribute('type', 'submit');
+  
+   formConfig.forEach(config => {
+    if (config.element === 'text' && config.name === 'login') {
+      createInputs(formInputLogin, formInputAge);
+    }
+    if (config.element === 'select') {
+      createSelectAndOption(formSelect);
+    }
+
+  })
+  form.addEventListener('submit', handleSubmit);
+  form.append(formInputLogin, formInputAge, formSelect, button);
+  document.body.append(form);
+}
+createForm()
+
+function createInputs(formInputLogin, formInputAge) {
+
+  formConfig.forEach(config => {
+    config.element === 'text';
+  let inputs = document.createElement('input');
+  let inputsLabel = document.createElement('label');
+
+  inputs.setAttribute('type', config.element);
+  inputsLabel.setAttribute('for', config.name);
+  inputsLabel.innerHTML = config.label;
+  inputs.setAttribute('id', config.name);
+
+  if (config.name === 'login') {
+     formInputLogin.append(inputsLabel, inputs);
+  };
+
+  if (config.name === 'age') {
+     formInputAge.append(inputsLabel, inputs);
+  };
+  })
+};
+
+
+
+
+
+
+
+
+function createSelectAndOption(formSelect) {
+  const findSelectElement = formConfig.find((config) => config.element === 'select');
+  let languageSelect = document.createElement('select');
+  let selectLabel = document.createElement('label');
+
+  selectLabel.setAttribute('for', findSelectElement.name);
+  selectLabel.innerHTML = findSelectElement.label;
+  languageSelect.setAttribute('id', findSelectElement.name);
+  findSelectElement.options.forEach(option => {
+    option.text;
+    const selectOptionJs = document.createElement('option');
+    selectOptionJs.innerHTML = option.text;
+    selectOptionJs.setAttribute('value', option.value);
+    languageSelect.append(selectOptionJs);
+  })
+
+  return formSelect.append(selectLabel, languageSelect);
+};
+
+
+
+
+
+
 
 
 
